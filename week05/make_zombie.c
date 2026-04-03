f0c484280527da7c7e48866f9d2a26cbbf2abe4f
윤치혁 <chyoon0308@sungkyul.ac.kr>
2026-04-03 10:05:55 +0900

    week05 완료: 프로세스 관리 실습

---
+++ week05/make_zombie.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+int main() {
+    pid_t pid = fork();
+    if (pid == 0) {
+        printf("자식 PID %d: 3초 후 종료합니다\n", getpid());
+        sleep(3);
+        printf("자식 종료\n");
+        _exit(0);
+    } else if (pid > 0) {
+        printf("부모 PID %d: 자식 %d 생성함\n", getpid(), pid);
+        printf("부모는 wait() 하지 않고 무한 대기 중...\n");
+        while (1) { sleep(1); }
+    } else {
+        perror("fork 실패");
+        return 1;
+    }
+    return 0;
+}
